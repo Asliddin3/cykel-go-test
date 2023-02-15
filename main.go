7577e5e36a77b16131412f046c4cab0b4dcaad16
Asliddin3 <asliddindeh@mail.ru>
2023-02-15 21:17:45 +0500

    start test

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ const (
 )
 
 func main() {
-	// Listen for incoming connections.
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
@@ -36,20 +35,18 @@ func main() {
 
 // Handles incoming requests.
 func handleRequest(conn net.Conn) {
-	// Make a buffer to hold incoming data.
-	var buf []byte
-	// Read the incoming connection into the buffer.
-	_, err := conn.Read(buf)
+	buf := make([]byte, 2048)
+
+	lenBuf, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
-	fmt.Println("read result", string(buf))
-	// Send a response back to person contacting us.
-	res := addByte([]byte("*CMDS,OM,860537062636022,200318123020,L0,0,1234,1497689816#\r\n"))
+	fmt.Println("read result", string(buf), "with len", lenBuf)
+
+	res := addByte([]byte("*CMDS,OM,860537062636022,200318123020,L0,0,1234,1497689816#\n"))
 	fmt.Println("send message", string(res))
 	_, err = conn.Write([]byte(res))
 	fmt.Println("write error", err)
-	// Close the connection when you're done with it.
 	conn.Close()
 }
 func addByte(b2 []byte) []byte {
