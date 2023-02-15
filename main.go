3542f0dfeb30f3d3eed12e4d3d22dcc26a53d899
Asliddin3 <asliddindeh@mail.ru>
2023-02-15 12:59:42 +0500

    add

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 	conn, err := l.Accept()
 	fmt.Println(err)
-	_, err = conn.Write([]byte("0xFFFF*CMDS,OM,860537062636022,000000000000,L0,0,1234,1497689816#\n"))
+	_, err = conn.Write([]byte("0xFFFF*CMDS,OM,860537062636022,200318123020,L0,0,1234,1497689816#\n"))
 	fmt.Println("write error", err)
 	conn.Close()
 	conn1, err := l.Accept()
@@ -44,7 +44,7 @@ func main() {
 	if err != nil {
 		fmt.Println("some error accepting from lock", err)
 	}
-	conn2.Write([]byte("0xFFFF*CMDS,OM,860537062636022,000000000000,Re,L0#\n"))
+	conn2.Write([]byte("0xFFFF*CMDS,OM,860537062636022,200318123020,Re,L0#\n"))
 	conn2.Close()
 	// 	for {
 	// 		// Listen for an incoming connection.
